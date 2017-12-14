6e38169084b26471d9370e07e8f622cfa08d5cff
Kothapalli <553611@AMB00687.local>
2017-12-14 19:08:28 +0530

    added service name

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,19 +2,19 @@
 
 set -e +x
 
-pushd attendee-service-source
+pushd attendee-service
   echo "Packaging JAR"
   ./mvnw clean package -DskipTests
 popd
 
-jar_count=`find attendee-service-source/target -type f -name *.jar | wc -l`
+jar_count=`find attendee-service/target -type f -name *.jar | wc -l`
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found, don't know which one to deploy. Exiting"
   exit 1
 fi
 
-find attendee-service-source/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.jar \;
+find attendee-service/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.jar \;
 
 echo "Done packaging"
 exit 0
